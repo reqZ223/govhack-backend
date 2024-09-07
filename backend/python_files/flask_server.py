b545afc499a257c385a94894a9d2b80aac5a64aa
reqZ223 <u3241596@uni.canberra.edu.au>
2024-09-07 21:57:51 +1000

    adds backend code

---
+++ backend/python_files/flask_server.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from govHack import provide_output
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/submit', methods=['POST'])
+def rewrite_data():
+    # Get JSON data from the request
+    data = request.get_json()
+    question = data.get('question')
+    type = data.get('type')
+    print(question)
+    answer = provide_output(question, type)
+    print(answer)
+    return jsonify({"answer": answer}), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- backend/python_files/govHack.py
+++ backend/python_files/govHack.py
@@ -8,10 +8,15 @@ from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from datetime import datetime
 
+
 # Set API keys and environment variables
 def set_env_variables():
-    os.environ["OPENAI_API_KEY"] = "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"
-    os.environ["LANGCHAIN_API_KEY"] = "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"
+    os.environ["OPENAI_API_KEY"] = (
+        "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"
+    )
+    os.environ["LANGCHAIN_API_KEY"] = (
+        "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"
+    )
     os.environ["TAVILY_API_KEY"] = "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"
     os.environ["LANGCHAIN_TRACING_V2"] = "true"
     os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
@@ -27,8 +32,8 @@ def load_documents():
         "https://www.stylemanual.gov.au/grammar-punctuation-and-conventions/titles-honours-forms-address/diplomats",
         "https://www.stylemanual.gov.au/grammar-punctuation-and-conventions/titles-honours-forms-address/judiciary",
         "https://www.stylemanual.gov.au/grammar-punctuation-and-conventions/titles-honours-forms-address/parliaments-and-councils",
-        "https://www.stylemanual.gov.au/grammar-punctuation-and-conventions/titles-honours-forms-address/royalty-vice-royalty-and-nobility"
-    ]  
+        "https://www.stylemanual.gov.au/grammar-punctuation-and-conventions/titles-honours-forms-address/royalty-vice-royalty-and-nobility",
+    ]
     docs = [WebBaseLoader(url).load() for url in urls]
     docs_list = [item for sublist in docs for item in sublist]
     return docs_list
@@ -46,65 +51,69 @@ def split_documents(docs_list):
 def setup_vectorstore_and_retriever(doc_splits):
     vectorstore = SKLearnVectorStore.from_documents(
         documents=doc_splits,
-        embedding=HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+        embedding=HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-mpnet-base-v2"
+        ),
     )
     retriever = vectorstore.as_retriever(k=4)
     return retriever
 
 
+def get_prompts(type):
+    if type == "rewrite":
+        prompt = PromptTemplate(
+            template="""
+        Your function is to rewrite or paraphrase the text based on the type of text based on the Australian Government Style Manual. 
+        """, input_variables=["question", "documents"],
+        )
+    elif type == "feedback":
+        prompt = PromptTemplate(
+            """   
+            Your function is to provide feedback on the inputs. 
+
+            You will also give a scoring criteria (grammar, punctuation, titles, and honours) on a scale of 1 to 10 based on the Australian Government Style Manual. 
+            """, input_variables=["question", "documents"],
+        )
+
+    return prompt
+
+
 # Function to initialize the LLM and RAG chain
-def setup_rag_chain():
-    prompt = PromptTemplate(
-        template="""
-        
-        You are an assistant for question-answering tasks. 
-
-        Your functions are to paraphrase based on the Australian Government Style Manual. 
-
-        You must also cite the relevant sources without websites that you used to paraphrase. 
-        
-        If you don't know the answer, just say that you don't know. 
-        
-        Question: {question} 
-        Documents: {documents} 
-        Answer: 
-        """,
-        input_variables=["question", "documents"],
-    )
-
+def setup_rag_chain(type):
+    prompt_to_use = get_prompts(type)
+    print(prompt_to_use)
     llm = ChatOllama(
         model="llama3.1",
         temperature=0,
     )
 
-    return prompt | llm | StrOutputParser()
+    return prompt_to_use | llm | StrOutputParser()
 
 
 # Function to get an answer to a question based on retrieved documents
 def answer_question(question, retriever, rag_chain):
     # Retrieve relevant documents from the vector store based on the question
     retrieved_docs = retriever.get_relevant_documents(question)
-    
+
     # Concatenate the retrieved documents' content
     documents = "\n".join([doc.page_content for doc in retrieved_docs])
 
     # Define the input for the chain
-    input_values = {
-        "question": question,
-        "documents": documents
-    }
-
+    input_values = {"question": question, "documents": documents}
+    print(input_values)
     # Generate the output by running the chain with the input values
     output = rag_chain.invoke(input_values)
     return output
+
 
 def calculateDuration(start, end):
     duration = end - start
     duration_in_s = duration.total_seconds()
     return duration_in_s
 
+
 # Main function to initialize and run the processes
-def main():
+def provide_output(question, type):
     # Set environment variables
     start = datetime.now()
     set_env_variables()
@@ -128,29 +137,22 @@ def main():
     retriever = setup_vectorstore_and_retriever(doc_splits)
     end = datetime.now()
     duration_setup = calculateDuration(start, end)
-    
-    # Setup the RAG 
+    print(f"Setting up the retriever took {duration_setup} seconds")
+
+    # Setup the RAG
     start = datetime.now()
-    rag_chain = setup_rag_chain()
+    rag_chain = setup_rag_chain(type)
     end = datetime.now()
     duration_rag = calculateDuration(start, end)
+    print(f"Setting up the RAG chain took {duration_rag} seconds")
 
-    print(f"Setting up the retriever took {duration_setup} seconds")
-    question = '''
-    How should I address an academic or professional?
-    '''
+
+    question_to_answer = question
     # Get the answer
     start = datetime.now()
-    answer = answer_question(question, retriever, rag_chain)
-    end = datetime.now()   
+    answer = answer_question(question_to_answer, retriever, rag_chain)
+    end = datetime.now()
     duration_answer = calculateDuration(start, end)
     print(f"Answering the question took {duration_answer} seconds")
-    
-    # Print the answer
-    print("--------------------------------------------")
-    print(answer)
 
-
-# Run the main function
-if __name__ == "__main__":
-    main()
+    return answer
